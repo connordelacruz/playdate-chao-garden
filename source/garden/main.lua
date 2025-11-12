d60694b17063f966ff57db1fe52d062e95cac656
Connor de la Cruz <connor.c.delacruz@gmail.com>
2025-11-12 14:26:43 -0600

    Skeleton cursor + state utils commit

--- source/garden/main.lua
+++ source/garden/main.lua
@@ -1,4 +1,5 @@
 import 'garden/background'
+import 'garden/ui/cursor'
 import 'garden/ui/status-panel'
 
 -- ================================================================================
@@ -8,6 +9,8 @@ import 'garden/ui/status-panel'
 local bg = GardenBackground()
 -- Status Panel UI
 local statusPanel = StatusPanel(SCREEN_WIDTH - bg.width)
+-- Cursor
+local cursor = Cursor(SCREEN_WIDTH - 64, 64)
 
 -- Chao
 local gardenCenterX = 400 - (bg.width / 2)
--- source/garden/ui/cursor.lua
+++ source/garden/ui/cursor.lua
@@ -2,32 +2,34 @@ local pd <const> = playdate
 local gfx <const> = pd.graphics
 
 -- ===============================================================================
--- Chao Sprite Class
+-- Cursor Sprite Class
 -- ===============================================================================
 -- TODO: implement states
-class('Chao').extends(gfx.sprite)
+class('Cursor').extends(gfx.sprite)
 
-function Chao:init(startX, startY)
-    Chao.super.init(self)
+function Cursor:init(startX, startY)
+    Cursor.super.init(self)
     -- --------------------------------------------------------------------------------
     -- Spritesheet
     -- --------------------------------------------------------------------------------
-    self.spritesheet = gfx.imagetable.new('images/chao/chao-idle-walk')
-    self:setImage(self.spritesheet[1])
+    self.spritesheet = gfx.imagetable.new('images/ui/cursor')
+    -- Spritesheet indices
+    self.pointerSpriteIndex = 1
+    self.grabSpriteIndex = 2
+    self.hoverSpriteIndex = 3
+    self.petSpriteIndex = 4
+    self:setImage(self.spritesheet[self.pointerSpriteIndex])
     -- --------------------------------------------------------------------------------
     -- Collision
     -- --------------------------------------------------------------------------------
     self:setCollideRect(0, 0, self:getSize())
+    self.collisionResponse = gfx.sprite.kCollisionTypeOverlap
     -- --------------------------------------------------------------------------------
     -- Initialization
     -- --------------------------------------------------------------------------------
+    -- Cursor should appear above most sprites
+    self:setZIndex(999)
+
     self:moveTo(startX, startY)
     self:add()
-end
-
-function Chao:update()
-    local ms = pd.getCurrentTimeMilliseconds()
-    local spriteIndex = (ms // 500 % #self.spritesheet) + 1
-    print(spriteIndex)
-    self:setImage(self.spritesheet[spriteIndex])
 end

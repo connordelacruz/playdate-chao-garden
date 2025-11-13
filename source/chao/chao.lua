d6e67491c9c248a22cb5fb7335cc4618f8f1702b
Connor de la Cruz <connor.c.delacruz@gmail.com>
2025-11-13 13:45:47 -0600

    added right-facing sprites and way to pick sprite based on action

--- source/chao/chao.lua
+++ source/chao/chao.lua
@@ -2,6 +2,19 @@ local pd <const> = playdate
 local gfx <const> = pd.graphics
 
 -- ===============================================================================
+-- Constants
+-- ===============================================================================
+-- Directions Chao can face
+local kDown <const>  = 'down'
+local kLeft <const>  = 'left'
+local kUp <const>    = 'up'
+local kRight <const> = 'right'
+-- Actions Chao can perform at each index
+local kIdle <const>  = 'idle'
+local kLStep <const> = 'lStep'
+local kRStep <const> = 'rStep'
+
+-- ===============================================================================
 -- Chao Sprite Class
 -- ===============================================================================
 -- TODO: implement states
@@ -13,7 +26,20 @@ function Chao:init(startX, startY)
     -- Spritesheet
     -- --------------------------------------------------------------------------------
     self.spritesheet = gfx.imagetable.new('images/chao/chao-idle-walk')
-    self:setImage(self.spritesheet[1])
+    -- Index starts for directions
+    self.spriteDir = {
+        [kDown]  = 1,
+        [kLeft]  = 4,
+        [kUp]    = 7,
+        [kRight] = 10,
+    }
+    -- Index modifiers to add to direction for idle and stepping
+    self.spriteAction = {
+        [kIdle]  = 0,
+        [kLStep] = 1,
+        [kRStep] = 2,
+    }
+    self:setImage(self:spritesheetImage(kDown, kIdle))
     -- --------------------------------------------------------------------------------
     -- Collision
     -- --------------------------------------------------------------------------------
@@ -23,6 +49,12 @@ function Chao:init(startX, startY)
     -- --------------------------------------------------------------------------------
     self:moveTo(startX, startY)
     self:add()
+end
+
+function Chao:spritesheetImage(dir, action)
+    local dirIndex = self.spriteDir[dir]
+    local actionIndex = self.spriteAction[action]
+    return self.spritesheet[dirIndex + actionIndex]
 end
 
 function Chao:update()

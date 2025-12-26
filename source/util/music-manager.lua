f48f1ce42736c8ce29fa49296420370b55feb1d7
Connor de la Cruz <connor.c.delacruz@gmail.com>
2025-12-26 14:07:34 -0600

    save music playback preference

--- source/util/music-manager.lua
+++ source/util/music-manager.lua
@@ -11,6 +11,8 @@ local kTrackPaths <const> = {
 }
 -- Volume level to set player to when not muted.
 local kPlayerVolume <const> = 0.8
+-- Filename where playback prefs are stored
+local kDataFilename <const> = 'music-prefs-data'
 
 -- ===============================================================================
 -- Music Manager Class
@@ -20,7 +22,12 @@ class('MusicManager').extends()
 function MusicManager:init()
     self.player = pd.sound.fileplayer.new()
     self.player:setVolume(kPlayerVolume)
-    -- TODO: save/load music playback setting
+    -- Load saved music playback prefs
+    self:loadData()
+    -- Register save function
+    DATA_MANAGER:registerSaveFunction(kDataFilename, function ()
+        self:saveData()
+    end)
     -- Menu item for toggling music playback
     self:registerMenuItem()
 end
@@ -49,9 +56,11 @@ function MusicManager:stop()
 end
 
 function MusicManager:loadAndPlay(path)
-    self.player:stop()
+    self:stop()
     self.player:load(path)
-    self:play()
+    if self.playMusic then
+        self:play()
+    end
 end
 
 function MusicManager:mute()
@@ -67,6 +76,7 @@ function MusicManager:unmute(play)
 end
 
 function MusicManager:togglePlayback(play)
+    self.playMusic = play
     if play then
         self:unmute(true)
     else
@@ -75,14 +85,13 @@ function MusicManager:togglePlayback(play)
 end
 
 -- --------------------------------------------------------------------------------
--- System Menu Options
+-- System Menu Options + Save/Load Prefs
 -- --------------------------------------------------------------------------------
 
 function MusicManager:registerMenuItem()
     local menu = pd.getSystemMenu()
-    -- TODO: load saved prefs, use that as default
     local toggleMusicMenuItem, error = menu:addCheckmarkMenuItem(
-        'play music', true, function (val)
+        'play music', self.playMusic, function (val)
             DEBUG_MANAGER:vPrint('MusicManager: Music playback set to ' .. tostring(val))
             self:togglePlayback(val)
         end
@@ -91,6 +100,23 @@ function MusicManager:registerMenuItem()
         DEBUG_MANAGER:vPrint('MusicManager: Unable to add music playback menu item:')
         DEBUG_MANAGER:vPrint(error, 1)
     end
+end
+
+function MusicManager:saveData()
+    local data = {playMusic = self.playMusic}
+    DEBUG_MANAGER:vPrint('MusicManager:saveData() called. Data:')
+    DEBUG_MANAGER:vPrintTable(data)
+    pd.datastore.write(data, kDataFilename)
+end
+
+function MusicManager:loadData()
+    local loadedData = pd.datastore.read(kDataFilename)
+    if loadedData == nil then
+        DEBUG_MANAGER:vPrint('MusicManager: no save data found.')
+    else
+        DEBUG_MANAGER:vPrint('MusicManager: save data found. Play music setting = ' .. tostring(loadedData.playMusic))
+    end
+    self:togglePlayback(loadedData.playMusic)
 end
 
 -- --------------------------------------------------------------------------------
